6a5ab5997fba3cab82424d8f900c53d04e88b7d0
Bo Peng <ben.bog@gmail.com>
2018-12-24 21:53:20 -0600

    Remove testExecuteIPynb

--- test/test_execute.py
+++ test/test_execute.py
@@ -1198,12 +1198,6 @@ run: expand = True
         Base_Executor(wf).run()
 
 
-    def testExecuteIPynb(self):
-        '''Test extracting and executing workflow from .ipynb files'''
-        script = SoS_Script(filename='sample_workflow.ipynb')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-
     def testOutputReport(self):
         '''Test generation of report'''
         if os.path.isfile('report.html'):
